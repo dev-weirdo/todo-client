01b10022433ae69cc9e642d04e0451ee9ccd1776
dev-weirdo <dev_weirdo@protonmail.com>
2022-07-01 23:25:36 +0600

    calendar responsive

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -5,7 +5,8 @@ import 'react-calendar/dist/Calendar.css'
 const Calender = () => {
   return (
     <div className='min-h-screen flex items-center justify-center'>
-      <div className='w-96 bg-slate-300 p-5 rounded-lg'>
+      <div className='w-96 bg-slate-300 p-5 rounded-lg space-y-5'>
+        <h1 className='text-3xl underline font-light text-center'>Calendar</h1>
         <Calendar></Calendar>
       </div>
     </div>
